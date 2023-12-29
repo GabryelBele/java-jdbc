337778d5e9950b762e816986cfc43049c3ab2400
Gabryel Bele <belegabryel19@gmail.com>
2023-12-28 22:53:08 -0300

    Criando a ConnectionFactory

---
+++ src/lojaVirtualRepository/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package lojaVirtualRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public Connection recuperarConexao() throws SQLException {
+		return DriverManager
+				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&servertimezone=UTC",
+				"root", "root");
+	}
+}
--- src/lojaVirtualRepository/TestaConexao.java
+++ src/lojaVirtualRepository/TestaConexao.java
@@ -1,18 +1,17 @@
 package lojaVirtualRepository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestaConexao {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&servertimezone=UTC",
-		"root", "root");
+		ConnectionFactory conexao = new ConnectionFactory();
+		Connection conn = conexao.recuperarConexao();
 		
 		System.out.println("Fechando conexão");
 		
-		connection.close();
+		conn.close();
 
 	}
 
