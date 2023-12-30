df538cb9c888191aa7546d6cf14674832dc81217
Gabryel Bele <belegabryel19@gmail.com>
2023-12-29 23:10:37 -0300

    Lidando com rollback e commit

--- src/lojaVirtualRepository/TestainsercaoComparada.java
+++ src/lojaVirtualRepository/TestainsercaoComparada.java
@@ -9,26 +9,41 @@ import java.sql.Statement;
 public class TestainsercaoComparada {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "mouse'";
-		String descricao = "mouse sem o(culos";
-		
-		ConnectionFactory conexao = new ConnectionFactory();
-		Connection conn = conexao.recuperarConexao();
+        String nome = "mouse'";
+        String descricao = "mouse sem o(culos";
+       
+        ConnectionFactory conexao = new ConnectionFactory();
+        Connection conn = conexao.recuperarConexao();
 
-		PreparedStatement result = 
-				conn.prepareStatement("INSERT INTO produto (nome, descricao) VALUES (?, ?)",
-						Statement.RETURN_GENERATED_KEYS);
-		
-		result.setString(1, nome);
-		result.setString(2, descricao);
-		
-		result.execute();		
-		ResultSet rst = result.getGeneratedKeys();
-		
-		while(rst.next()) {
+        conn.setAutoCommit(false);
+        
+       try {
+           PreparedStatement result = conn.prepareStatement("INSERT INTO produto (nome, descricao) VALUES (?, ?)",
+                   Statement.RETURN_GENERATED_KEYS);
+
+           adicionarVariavel(nome, descricao, result);
+           adicionarVariavel(nome, descricao, result);
+           
+           conn.commit();
+           
+           result.close();
+           conn.close();
+	} catch (Exception e) {
+		e.printStackTrace();
+		conn.rollback();
+	}
+    }
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+
+		stm.execute();
+		ResultSet rst = stm.getGeneratedKeys();
+
+		while (rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O id inserido foi: " + id);
 		}
-
 	}
 }
