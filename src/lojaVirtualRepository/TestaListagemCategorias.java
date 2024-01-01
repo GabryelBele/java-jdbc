1ba0e2cf2bc5178ffa752b14c13a8bf4f174efbc
Gabryel Bele <belegabryel19@gmail.com>
2024-01-01 20:12:51 -0300

    Buscando produto por categoria

--- src/lojaVirtualRepository/TestaListagemCategorias.java
+++ src/lojaVirtualRepository/TestaListagemCategorias.java
@@ -4,22 +4,35 @@ import java.sql.Connection;
 import java.util.List;
 
 import lojaVirtualRepository.dao.CategoryDao;
+import lojaVirtualRepository.dao.ProdutoDao;
 import lojaVirtualRepository.models.Categoria;
+import lojaVirtualRepository.models.Produto;
 
 public class TestaListagemCategorias {
 
 	public static void main(String[] args) {
 
-		try(Connection conn = new ConnectionFactory().recuperarConexao()) {
+		try (Connection conn = new ConnectionFactory().recuperarConexao()) {
+			
 			CategoryDao categoryDao = new CategoryDao(conn);
 			
 			List<Categoria> list = categoryDao.listar();
-			list.stream().forEach(result -> System.out.println(result.getNome()));
-			
+			list.stream().forEach(result -> {
+				System.out.println(result.getNome());
+
+				try {
+					for (Produto produto : new ProdutoDao(conn).buscar(result)) {
+						System.out.println(result.getNome() + " - " + produto.getNome());
+					}
+				} catch (Exception e) {
+					e.getMessage();
+				}
+			});
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
--- src/lojaVirtualRepository/dao/ProdutoDao.java
+++ src/lojaVirtualRepository/dao/ProdutoDao.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import lojaVirtualRepository.models.Categoria;
 import lojaVirtualRepository.models.Produto;
 
 public class ProdutoDao {
@@ -57,7 +58,28 @@ public class ProdutoDao {
 
 			}
 		}
-		
+
+		return produtos;
+	}
+
+	public List<Produto> buscar(Categoria result) throws SQLException {
+		List<Produto> produtos = new ArrayList<Produto>();
+
+		String sql = "SELECT id, nome,descricao FROM produto WHERE categoria_id = ? ";
+
+		try (PreparedStatement stm = connection.prepareStatement(sql);) {
+			stm.setInt(1, result.getId());
+			stm.execute();
+
+			ResultSet rst = stm.getResultSet();
+
+			while (rst.next()) {
+				Produto produto = new Produto(rst.getInt(1), rst.getString(2), rst.getString(3));
+				produtos.add(produto);
+
+			}
+		}
+
 		return produtos;
 	}
 
